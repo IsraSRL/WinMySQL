6c2c7f49e00085f6595dad029eb31359f4171046
Isra <kevig600@gmail.com>
2026-02-23 15:48:56 -0600

    Agregar archivos de proyecto.

---
+++ WinMySQL/Clases/Datos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace WinMySQL.Clases
+{
+    internal class Datos
+    {
+        string cadenaConexion="server=localhost;user=luis;pwd=joseluis;";
+        MySqlConnection conexion;
+
+        private void Conectar()
+        {
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Desconectar()
+        {
+            try
+            {
+                if (conexion != null)
+                {
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+    }
+}
